bb75743b2cdf032224f4e41e6d8f63b494d96d13
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-09-30 15:44:18 +0900

    0101完成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
-#include<windows.h>
+#include <list>
+using namespace std;
+
 
 int main() {
-	//SetConsoleOutputCP(65001);
-	
-	char str[] = "ソ";
-	printf("%s\n", str);
-	return 0;
+    // 1970年の山手線駅
+    list<const char*> lst{
+    "Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome",
+    "Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+    "Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi","Yurakucho"
+    };
+
+    printf("1970\n");
+    for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+        cout << *itr;
+        auto next = itr;
+        ++next;
+        if (next != lst.end())
+            cout << " - ";
+
+
+
+    }
+    printf("\n\n");
+
+	printf("2019\n");
+    
+
+
+	for (std::list<const char*>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {
+        if (*itr == "Tabata")
+        {
+            itr = lst.insert(itr, "Nisi-Nippori");
+            ++itr;
+        }
+		
+	}   
+
+
+     for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+         cout << *itr;
+         auto next = itr;
+         ++next;
+         if (next != lst.end())
+             cout << " - ";
+    }
+
+
+     printf("\n\n");
+
+     printf("2022\n");
+     for (std::list<const char*>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {
+         if (*itr == "Tamachi")
+         {
+             itr = lst.insert(itr, "Takanawa.Gateway");
+             ++itr;
+         }
+
+     }
+
+
+     for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+         cout << *itr;
+         auto next = itr;
+         ++next;
+         if (next != lst.end())
+             cout << " - ";
+
+     }
+     printf("\n\n");
+    return 0;
 }
