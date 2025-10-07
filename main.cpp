1ad2feb02f0e06636a10f530ff2afa0767af97dd
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-10-07 12:27:36 +0900

    02_01完成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include<windows.h>
 
+template<typename Type>
+Type Min(Type a, Type b) {
+	if(a<b)
+	{
+	return static_cast<Type>(a);
+	}
+	else
+	{
+		return static_cast<Type>(b);
+	}
+}
+
 int main() {
-	//SetConsoleOutputCP(65001);
 	
-	char str[] = "ソ";
-	printf("%s\n", str);
+	printf("%d\n", Min<int>(3, 8));
+
+	printf("%f\n", Min<float>(8.9f, 9.9f));
+	printf("%f\n", Min<double>(3.141592, 4.2526037));
+
+
 	return 0;
 }
