9f9aa21e29f47580e46470701ff20264f53610b7
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-10-14 12:25:31 +0900

    02_02完成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
+#include <stdio.h>
 #include <iostream>
-#include<windows.h>
+#include <windows.h>
 
-int main() {
-	//SetConsoleOutputCP(65001);
-	
-	char str[] = "ソ";
-	printf("%s\n", str);
-	return 0;
+int Recursive(int kyuuryou, int zikan, int koteikyu)
+{
+    if (kyuuryou >= koteikyu)
+    {
+        printf("%d時間後に超える\n", zikan);
+        return zikan;
+    }
+
+    printf("%d時給　%d固定給\n", kyuuryou, koteikyu);
+
+    // 毎時間給料が2倍になる
+    kyuuryou = kyuuryou * 2 - 50;
+
+    // 固定給
+    koteikyu += 1072;
+
+    return Recursive(kyuuryou, zikan + 1, koteikyu);
+}
+
+int main()
+{
+
+
+    int saiki = 100;
+    int ippan = 1226;
+    int zikan = 0;
+    int result1 = Recursive(saiki, zikan, ippan);
+
+    printf("\n結果: %d時間後\n", result1);
+
+    return 0;
 }
