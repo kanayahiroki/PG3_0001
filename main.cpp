8fc655cede32f78eae5d956c9bae484e155b466d
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-10-21 11:44:52 +0900

    02_03完成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <windows.h>
 
+// コールバック関数のプロトタイプ宣言
+typedef void (*Callback)(int result);
+
+// サイコロの出目を決定する関数
+int roll_dice() {
+    return (rand() % 6) + 1; // ランダム関数で1～6のどれかを返す
+}
+
+// 判定を行うコールバック関数
+void judge_result(int result) {
+
+    int input;
+
+    printf("サイコロの出目が奇数(1)か偶数(2)か入力:");
+    scanf_s("%d", &input);
+
+    printf("判定中...\n");
+
+    Sleep(3000);
+
+    if ((result % 2 == 1 && input == 1) || (result % 2 == 0 && input == 2)) {
+        printf("正解\n");
+    } else {
+        printf("不正解\n");
+    }
+
+    printf("サイコロの目は%d\n", result);
+}
+
+// メイン関数
 int main() {
-	//SetConsoleOutputCP(65001);
-	
-	char str[] = "ソ";
-	printf("%s\n", str);
-	return 0;
+
+    srand((int)time(NULL));
+
+    int dice_result = roll_dice();
+
+    Callback callback = judge_result; // 実行したい関数のアドレス（関数名がアドレスを表す）
+
+    callback(dice_result); // サイコロの値を引数にして関数ポインタから関数を実行
+
+    return 0;
 }
