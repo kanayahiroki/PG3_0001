12ab282f2e1fc7776637530b8a5abeb38aca66ac
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-11-11 11:29:26 +0900

    0303完了

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
-#include <iostream>
-#include <windows.h>
+#include <cstdio>
+
+using namespace std;
+
+class Animal {
+public:
+    // 鳴く関数（純粋仮想関数）
+    virtual void sound() const = 0;
+
+	// 仮想デストラクタ
+	virtual ~Animal() {}
+};
+
+
+class Dog : public Animal {
+public:
+    void sound() const override {
+        printf("イヌ鳴き声：ワン\n");
+    }
+};
+
+
+class Cat : public Animal {
+public:
+    void sound() const override {
+        printf("ネコ鳴き声:ニャー\n");
+    }
+};
 
 int main() {
-	//SetConsoleOutputCP(65001);
-	
-	char str[] = "ソ";
-	printf("%s\n", str);
-	return 0;
+
+    Animal* animal1 = new Dog();
+    Animal* animal2 = new Cat();
+
+    animal1->sound();
+    animal2->sound();
+
+    delete animal1;
+    delete animal2;
+
+    return 0;
 }
