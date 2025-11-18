adc00d05c3dffa5cf43bd19bf96802cca79cebf4
金谷浩樹 <k024g0041@g.neec.ac.jp>
2025-11-18 11:39:37 +0900

    owari

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(void) {
 	Shapes* shape[2];
 
 	shape[0] = new Circle();
-	shape[1] = new Square;
+	shape[1] = new Square();
 
 	printf("円の半径:4\n短形の底辺:2\n短形の高さ:3\n");
 
@@ -21,6 +21,10 @@ int main(void) {
 	//面積を画面に表示
 	for (int i = 0; i < 2; i++)
 		shape[i]->Draw();
+
+	//動的確保したメモリを解放
+	for (int i = 0; i < 2; i++)
+		delete shape[i];
 
 	return 0;
 }
